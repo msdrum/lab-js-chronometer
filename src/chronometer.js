9f73c9fd0b19e0502393f20f194f9446bb3474b4
Marcio Silva <marciosilvadrum@gmail.com>
2022-10-25 16:14:42 -0300

    iteration 1 to 5 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,47 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0; //property
+    this.intervalId = null; //property
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    //printTimeCallback é uma função que vem do index.js
+    //precisamos checar se essa função foi realmente passada
+    //CASO a função tenha sido passada ->>>> vamos invocar a função
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      //checando se printTimeCallback existe
+      if (printTimeCallback) {
+        //caso exista, invoque a função
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    // receber o total de minutos
+    const totalMinutes = this.currentTime / 60;
+    return Math.floor(totalMinutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    // receber o total de secundos, apenas os números quebrados, após os minutos ex: 210 s = 3 min e 30 sec.
+    const totalSeconds = this.currentTime % 60;
+    return Math.floor(totalSeconds);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // should always return a string of length 2
+    const towDigit = String(value);
+    let newString;
+    if (towDigit.length < 2) {
+      newString = `0${towDigit}`;
+      return newString;
+    }
+    return towDigit;
   }
 
   stop() {
